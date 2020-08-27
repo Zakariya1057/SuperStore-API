c20d222b55f11aca327469737137c6e717a1d8b3
Zakariya Mohummed <zakaria2011@live.no>
2020-08-27 12:48:30 +0100

    Empty Stores Trait

---
+++ app/Traits/StoreTrait.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Traits;
+
+use App\Store;
+
+trait StoreTrait {
+
+    // protected function store_details($store_id){
+    //     $store = Store::where('id',$store_id)->first();
+    //     return $store;
+    // }
+
+    // protected function store_facilities($store_id){
+    //     $store = Store::where('id',$store_id)->first();
+    //     return $store;
+    // }
+
+}
+
+?>
