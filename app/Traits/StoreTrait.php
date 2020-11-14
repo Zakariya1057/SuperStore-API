043368b4d66af4a11d4c2d04966b4362ce98569f
Zakariya Mohummed <zakaria2011@live.no>
2020-11-14 12:59:44 +0000

    Store trait, return hours as property array

--- app/Traits/StoreTrait.php
+++ app/Traits/StoreTrait.php
@@ -36,15 +36,6 @@ trait StoreTrait {
         ->join('store_types', 'store_types.id', '=', 'stores.store_type_id')
         ->join('store_locations','store_locations.store_id', '=','stores.id');
 
-        if($opening_hours){
-            $day_of_week = Carbon::now()->dayOfWeek == 0 ? 6 : Carbon::now()->dayOfWeek - 1;
-
-            array_push($select, 'closes_at','opens_at');
-            $query_builder = $query_builder->join('opening_hours', function ($join) use($day_of_week) {
-                $join->on('opening_hours.store_id', '=', 'stores.id')->where('day_of_week', '=', $day_of_week);
-            });
-        } 
-
         $location_fields = [
             'city',
             'address_line1',
@@ -55,11 +46,33 @@ trait StoreTrait {
             'longitude',
         ];
 
+        $hours_fields = ['opens_at','closes_at','day_of_week','closed_today'];
+
+        if($opening_hours){
+            $day_of_week = Carbon::now()->dayOfWeek == 0 ? 6 : Carbon::now()->dayOfWeek - 1;
+            $select = array_merge($select, $hours_fields);
+
+            $query_builder = $query_builder->join('opening_hours', function ($join) use($day_of_week) {
+                $join->on('opening_hours.store_id', '=', 'stores.id')->where('day_of_week', '=', $day_of_week);
+            });
+        } 
+
         $stores = $query_builder->select($select)->withCasts($casts)->get();
 
         foreach($stores as $store){
             $location = [];
             
+            if($opening_hours){
+                $hour = new OpeningHour();
+
+                foreach($hours_fields as $field){
+                    $hour->{$field} = $store->{$field};
+                    unset($store->{$field});
+                }
+    
+                $store->opening_hours = [$hour];
+            }
+
             foreach($location_fields as $field){
                 $location[$field] = $store->{$field};
                 unset($store->{$field});
