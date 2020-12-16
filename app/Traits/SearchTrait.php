3457c569571487841d53247ec81d2d003a4b81e3
Zakariya Mohummed <zakaria2011@live.no>
2020-12-16 11:39:11 +0000

    Improved elastic searching

--- app/Traits/SearchTrait.php
+++ app/Traits/SearchTrait.php
@@ -12,45 +12,42 @@ trait SearchTrait {
         $query = strtolower($query);
 
         if($index == 'products'){
-            $fields = [
-                'name',
-                'description',
-                'brand'
-            ];
+            $fields_match = ['name','description','brand','dietary_info'];
+            $fields_should = ['name', 'weight','brand'];
         } else {
-            $fields = [
-                'name'
-            ];
+            $fields_match = ['name'];
+            $fields_should = ['name', 'weight'];
         }
 
         $params = [
             'index' => $index,
             'body'  => [
                 'size' => $limit,
-                'query' => [
-                    'bool' => [
-                    'must' => [
-                        [
-                        'multi_match' => [
-                            'query' => $query,
-                            'fields' => $fields,
-                            'operator' => 'and',
-                            'fuzziness' => 'auto'
-                        ]
-                        ]
-                    ],
+                
+                    'query' => [
+
+                        'bool' => [
+
+                        'must' => [
+                            [
+                                'multi_match' => [
+                                    'query' => $query,
+                                    'fields' => $fields_match,
+                                    'operator' => 'or',
+                                    'fuzziness' => 'auto'
+                                ]
+                            ]
+                        ],
                     
-                    'should' => [
-                        [
-                        'multi_match' => [
-                            'query' => $query,
-                            'fields' => [
-                                'name'
-                            ],
-                            'operator' => 'and'
+                        'should' => [
+                            [
+                                'multi_match' => [
+                                    'query' => $query,
+                                    'fields' => $fields_should,
+                                    'operator' => 'and'
+                                ]
+                            ]
                         ]
-                        ]
-                    ]
                     ]
                 ]
             ]
