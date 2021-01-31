33fe4f045be75622d912962aaa94b39ed3e9d2f0
Zakariya Mohummed <zakaria2011@live.no>
2021-01-31 21:10:48 +0000

    Promotion Trait Improvements

--- app/Traits/PromotionTrait.php
+++ app/Traits/PromotionTrait.php
@@ -3,13 +3,23 @@
 namespace App\Traits;
 
 use App\FeaturedItem;
-use App\Promotion;
+use App\Services\PromotionService;
 
 trait PromotionTrait {
 
     protected function store_promotions($store_id){
-        $promotion = new Promotion();
-        return FeaturedItem::select('promotions.id as promotion_id', 'name as promotion')->whereRaw('type = "promotions"')->join('promotions','promotions.id','featured_id')->withCasts($promotion->casts)->limit(10)->get()->pluck('promotion')->toArray();
+
+        $promotion_service = new PromotionService();
+
+        $promotions_list = FeaturedItem::select('promotions.id as promotion_id', 'name as promotion')->whereRaw('type = "promotions"')->join('promotions','promotions.id','featured_id')->limit(10)->get();
+
+        $promotion_results = [];
+
+        foreach($promotions_list as $promotion){
+            $promotion_results[] = $promotion_service->details( $promotion );
+        }
+
+        return response()->json(['data' => $promotion_results]);
     }
 
 }
