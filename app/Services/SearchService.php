88a0023ae12ed35c31ee434d3cf963382c21241a
Zakariya Mohummed <zakaria2011@live.no>
2021-02-24 15:16:07 +0000

    Search Service Further Improvements

--- app/Services/SearchService.php
+++ app/Services/SearchService.php
@@ -9,6 +9,8 @@ use App\Models\StoreType;
 use Elasticsearch\Client;
 use Elasticsearch\ClientBuilder;
 use Exception;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Redis;
 
@@ -21,8 +23,10 @@ class SearchService {
         $this->store_service = $store_service;
     }
 
-    public function suggestions($query){
+    ///////////////////////////////////////////     Suggestions     ///////////////////////////////////////////
 
+    public function suggestions($query){
+        
         $results = [
             'stores' => [],
             'parent_categories' => [],
@@ -32,66 +36,18 @@ class SearchService {
 
         $cache_key = 'search_suggestions_' . str_replace(' ','_', $query);
 
-        if(Redis::get($cache_key)){
-            $results = json_decode(Redis::get($cache_key));
+        $cached_results = Redis::get($cache_key);
+
+        if($cached_results){
+            $results = json_decode($cached_results);
         } else {
 
             try {
-
-                $types = [
-                    'stores' => 2, 
-                    'categories' => 3, 
-                    'products' => 8
-                ];
-    
-                $total_items = 0;
-
-                foreach($types as $type => $limit){
-                    $response = $this->elastic_search($this->client, $type, $query, $type == 'products' && $total_items <= 3 ? 12 : $limit);
-                    $results[$type] = [];
-    
-                    $item_type = $type;
-                    $unique_names = [];
-
-                    foreach($response['hits']['hits'] as $item){
-                        $source = $item['_source'];
-                        $name = trim($source['name']);
-
-                        if($type == 'categories'){
-                            $item_type = $source['type'];
-                        }
-                        
-                        $total_items++;
-
-                        if(!key_exists($name, $unique_names)){
-                            $results[$item_type][] = ['id' => $source['id'], 'name' => $name];
-                            $unique_names[$name] = 1;
-                        }
-
-                    }
-
-                }
-    
+                $results = $this->suggestions_by_group($query, $results);
             } catch(Exception $e){
                 // Backup search in case elasticsearch fails from now
                 Log::critical('Elasticsearch Error: ' . $e);
-    
-                $stores = StoreType::select('id','name')->where('name', 'like', "%$query%")->groupBy('name')->limit(2)->get()->toArray();
-                $child_categories = ChildCategory::select('id','name')->where('name', 'like', "%$query%")->groupBy('name')->limit(5)->get()->toArray();
-                $parent_categories = ParentCategory::select('id','name')->where('name', 'like', "%$query%")->groupBy('name')->limit(5)->get()->toArray();
-    
-                if((count($child_categories) + count($parent_categories)) <= 3 ){
-                    $product_limit = 10;
-                } else {
-                    $product_limit = 5;
-                }
-    
-                $products = Product::select('id','name')->where('name', 'like', "%$query%")->groupBy('name')->orderByRaw('total_reviews_count / avg_rating desc')->limit($product_limit)->get()->toArray();
-        
-                $results['stores'] = $stores ?? [];
-                $results['parent_categories'] = $parent_categories ?? [];
-                $results['child_categories'] = $child_categories ?? [];
-                $results['products'] = $products ?? [];            
+                $results = $this->database_suggestions($query, $results);         
             }
 
             Redis::set($cache_key, json_encode($results));
@@ -102,8 +58,71 @@ class SearchService {
         return $results;
     }
 
-    public function results($data){
+    private function suggestions_by_group($query, $results){
+        $types = [
+            'stores' => 2, 
+            'categories' => 3, 
+            'products' => 8
+        ];
 
+        $total_items = 0;
+
+        foreach($types as $type => $limit){
+            $response = $this->elastic_search($this->client, $type, $query, $type == 'products' && $total_items <= 3 ? 12 : $limit);
+            $results[$type] = [];
+
+            $item_type = $type;
+            $unique_names = [];
+
+            foreach($response['hits']['hits'] as $item){
+                $source = $item['_source'];
+                $name = trim($source['name']);
+
+                if($type == 'categories'){
+                    $item_type = $source['type'];
+                }
+                
+                $total_items++;
+
+                if(!key_exists($name, $unique_names)){
+                    $results[$item_type][] = ['id' => $source['id'], 'name' => $name];
+                    $unique_names[$name] = 1;
+                }
+
+            }
+
+        }
+
+        return $results;
+    }
+
+    private function database_suggestions($query, $results){
+
+        $stores = StoreType::select('id','name')->where('name', 'like', "%$query%")->groupBy('name')->limit(2)->get()->toArray();
+        $child_categories = ChildCategory::select('id','name')->where('name', 'like', "%$query%")->groupBy('name')->limit(5)->get()->toArray();
+        $parent_categories = ParentCategory::select('id','name')->where('name', 'like', "%$query%")->groupBy('name')->limit(5)->get()->toArray();
+
+        if((count($child_categories) + count($parent_categories)) <= 3 ){
+            $product_limit = 10;
+        } else {
+            $product_limit = 5;
+        }
+
+        $products = Product::select('id','name')->where('name', 'like', "%$query%")->groupBy('name')->orderByRaw('total_reviews_count / avg_rating desc')->limit($product_limit)->get()->toArray();
+
+        $results['stores'] = $stores ?? [];
+        $results['parent_categories'] = $parent_categories ?? [];
+        $results['child_categories'] = $child_categories ?? [];
+        $results['products'] = $products ?? []; 
+        
+        return $results;
+    }
+
+    ///////////////////////////////////////////     Suggestions     ///////////////////////////////////////////
+
+
+    ///////////////////////////////////////////     Results        ///////////////////////////////////////////
+    public function results($data){
 
         $detail = $data['detail'];
         $type = strtolower($data['type']);
@@ -142,9 +161,10 @@ class SearchService {
             'products' => [],
             'filter' => null
         );
-
-        if(Redis::get($cache_key)){
-            $results = json_decode(Redis::get($cache_key));
+        
+        $cached_results = Redis::get($cache_key);
+        if($cached_results){
+            $results = json_decode($cached_results);
             Log::debug('Retrieved');
         } else {
             Log::debug('Fetched');
@@ -174,80 +194,21 @@ class SearchService {
                 ->withCasts($casts);
                 
                 if($text_search){
-                    if($type == 'products'){
-                        $base_query = $base_query->whereIn('products.id', $list_id);
-                    } elseif($type == 'child_categories'){
-                        $base_query = $base_query->where('child_categories.id',$list_id);
-                    } elseif($type == 'parent_categories'){
-                        $base_query = $base_query->where('parent_categories.id', $list_id);
-                    }
+                    $base_query = $this->text_search_where($list_id, $type, $base_query);
                 } else {
-                    if($type == 'products'){
-                        $base_query = $base_query->where('products.name', 'like', "$detail%");
-                    } elseif($type == 'child_categories'){
-                        $base_query = $base_query->where('child_categories.name',$detail);
-                    } elseif($type == 'parent_categories'){
-                        $base_query = $base_query->where('parent_categories.name', $detail);
-                    }
-                }
+                    $base_query = $this->search_where($type, $detail, $base_query);
+                }   
     
-                if(key_exists('sort', $data) && key_exists('order', $data)){
-                    $sort = strtolower($data['sort']);
-                    $order = strtoupper($data['order']);
-    
-                    if($order != 'ASC' && $order != 'DESC'){
-                        throw new Exception('Unknown order by option.', 422);
-                    }
-    
-                    $sort_options = [
-                        'rating' => 'avg_rating',
-                        'price' => 'price',
-                    ];
-    
-                    if(key_exists($sort,$sort_options)){
-                        $base_query = $base_query->orderByRaw($sort_options[$sort] . ' '. $order);
-                    } else {
-                        throw new Exception('Unknown sort by option.', 422);
-                    }
-    
-                } else {
-                    $base_query = $base_query->orderByRaw('total_reviews_count / avg_rating desc');
-                }
-    
-                if(key_exists('dietary', $data)){
-                    $dietary_list = explode(',',$data['dietary']);
+                $base_query = $this->search_sort($data, $base_query);
+                $base_query = $this->search_dietary($data, $base_query);
+                $base_query = $this->search_brand($data, $base_query);
+                $base_query = $this->search_category($data, $base_query);
 
-                    foreach($dietary_list as $dietary){
-                        $base_query = $base_query->where('dietary_info','like', "%$dietary%");
-                    }
-                    
-                }
+                $pagination_data = $this->paginate_results($base_query);
 
-                if(key_exists('brand', $data)){
-                    $brand = $data['brand'];
-                    $base_query = $base_query->where('brand',$brand);
-                }
-                
-                if(key_exists('child_category', $data)){
-                    $category = $data['child_category'];
-                    $base_query = $base_query->where('child_categories.name',$category);
-                }
-                
-                $paginator = $base_query->paginate(100);
+                $results['products'] = $pagination_data['products'];
+                $results['paginate'] = $pagination_data['paginate'];
 
-                $results['products'] = $paginator->items();
-
-                $results['paginate'] = [
-                    'from' => 0,
-                    'current' => $paginator->currentPage(),
-                    'to' => $paginator->lastPage(),
-                    'per_page' => $paginator->perPage(),
-                    'next_page_url' => $paginator->url( $paginator->currentPage() + 1),
-                    'current_page_url' => $paginator->url( $paginator->currentPage() ),
-                    'prev_page_url' => $paginator->previousPageUrl(),
-                    'more_available' => $paginator->hasMorePages(),
-                ];
-    
             }
 
             Redis::set($cache_key, json_encode($results));
@@ -258,6 +219,113 @@ class SearchService {
         return $results;
 
     }
+
+    private function search_where($type, $detail, Builder $base_query){
+        if($type == 'products'){
+            $base_query = $base_query->where('products.name', 'like', "$detail%");
+        } elseif($type == 'child_categories'){
+            $base_query = $base_query->where('child_categories.name',$detail);
+        } elseif($type == 'parent_categories'){
+            $base_query = $base_query->where('parent_categories.name', $detail);
+        }
+
+        return $base_query;
+    }
+
+    private function text_search_where($list_id, $type, Builder $base_query){
+        if($type == 'products'){
+            $base_query = $base_query->whereIn('products.id', $list_id);
+        } elseif($type == 'child_categories'){
+            $base_query = $base_query->where('child_categories.id',$list_id);
+        } elseif($type == 'parent_categories'){
+            $base_query = $base_query->where('parent_categories.id', $list_id);
+        }
+
+        return $base_query;
+    }
+    //////////////////    Filter Results   //////////////////
+
+    private function search_sort($data, Builder $base_query){
+        if(key_exists('sort', $data) && key_exists('order', $data)){
+            $sort = strtolower($data['sort']);
+            $order = strtoupper($data['order']);
+
+            if($order != 'ASC' && $order != 'DESC'){
+                throw new Exception('Unknown order by option.', 422);
+            }
+
+            $sort_options = [
+                'rating' => 'avg_rating',
+                'price' => 'price',
+            ];
+
+            if(key_exists($sort,$sort_options)){
+                $base_query = $base_query->orderByRaw($sort_options[$sort] . ' '. $order);
+            } else {
+                throw new Exception('Unknown sort by option.', 422);
+            }
+
+        } else {
+            $base_query = $base_query->orderByRaw('total_reviews_count / avg_rating desc');
+        }
+
+        return $base_query;
+    }
+
+    private function search_dietary($data, Builder $base_query){
+        if(key_exists('dietary', $data)){
+            $dietary_list = explode(',',$data['dietary']);
+
+            foreach($dietary_list as $dietary){
+                $base_query = $base_query->where('dietary_info','like', "%$dietary%");
+            }
+            
+        }
+
+        return $base_query;
+    }
+
+    private function search_brand($data, Builder $base_query){
+        if(key_exists('brand', $data)){
+            $brand = $data['brand'];
+            $base_query = $base_query->where('brand',$brand);
+        }
+
+        return $base_query;
+    }
+
+    private function search_category($data, Builder $base_query){
+        if(key_exists('child_category', $data)){
+            $category = $data['child_category'];
+            $base_query = $base_query->where('child_categories.name',$category);
+        }
+
+        return $base_query;
+    }
+
+    //////////////////     Filter Results   //////////////////
+
+    private function paginate_results(Builder $base_query, $limit = 100){
+        $results = [];
+
+        $paginator = $base_query->paginate($limit);
+
+        $results['products'] = $paginator->items();
+
+        $results['paginate'] = [
+            'from' => 0,
+            'current' => $paginator->currentPage(),
+            'to' => $paginator->lastPage(),
+            'per_page' => $paginator->perPage(),
+            'next_page_url' => $paginator->url( $paginator->currentPage() + 1),
+            'current_page_url' => $paginator->url( $paginator->currentPage() ),
+            'prev_page_url' => $paginator->previousPageUrl(),
+            'more_available' => $paginator->hasMorePages(),
+        ];
+
+        return $results;
+    }
+    ///////////////////////////////////////////     Results        ///////////////////////////////////////////
 
     private function elastic_search(Client $client, $index, $query, $limit=10): Array{
 
