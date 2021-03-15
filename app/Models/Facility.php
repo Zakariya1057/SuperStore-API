d86c77e0e5e92df1859baf5b2542f64b1129749b
Zakariya Mohummed <zakaria2011@live.no>
2021-03-15 19:30:40 +0000

    Facility Decode Name

--- app/Models/Facility.php
+++ app/Models/Facility.php
@@ -1,9 +1,12 @@
 <?php
 
 namespace App\Models;
+use App\Casts\HTMLDecode;
 
 use Illuminate\Database\Eloquent\Model;
 class Facility extends Model
 {
-    //
+    public $casts = [
+        'name' => HTMLDecode::class
+    ];
 }
