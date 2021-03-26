af8bd3fa8786021710e2ad62b17a84934f45cec8
Zakariya Mohummed <zakaria2011@live.no>
2021-03-26 09:36:07 +0000

    Store Services Closest Stores

--- app/Services/StoreService.php
+++ app/Services/StoreService.php
@@ -64,8 +64,7 @@ class StoreService {
             $select[] = DB::raw('( 6367 * acos( cos( radians('.$latitude.') ) * cos( radians( latitude ) ) * cos( radians( longitude ) - radians('.$longitude.') ) + sin( radians('.$latitude.') ) * sin( radians( latitude ) ) ) ) AS distance');
 
             $query_builder = $query_builder
-            // ->having('distance', '<', 50)
-            ->having('distance', '>', 0)
+            ->having('distance', '<', 50)
             ->orderBy('distance');
         }
 
