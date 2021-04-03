414e60ceb9d33f8c1883601d1ce3de4095db00cb
Zakariya Mohummed <zakaria2011@live.no>
2021-04-03 12:06:00 +0100

    Search Service/Category Service use RefineService

--- app/Services/CategoryService.php
+++ app/Services/CategoryService.php
@@ -11,11 +11,12 @@ use App\Models\FeaturedItem;
 
 class CategoryService {
     
-    private $promotion_service, $paginate_service;
+    private $promotion_service, $paginate_service, $refine_service;
 
-    public function __construct(PromotionService $promotion_service, PaginateService $paginate_service){
+    public function __construct(PromotionService $promotion_service, PaginateService $paginate_service, RefineService $refine_service){
         $this->paginate_service = $paginate_service;
         $this->promotion_service = $promotion_service;
+        $this->refine_service = $refine_service;
     }
     
     public function grand_parent_categories($store_type_id){
@@ -32,7 +33,7 @@ class CategoryService {
         return ChildCategory::where('child_categories.parent_category_id', $parent_category_id)->get();
     }
 
-    public function category_products($child_category_id){
+    public function category_products($child_category_id, $data = []){
         $product = new Product();
         $casts = $product->casts;
 
@@ -58,16 +59,15 @@ class CategoryService {
             'promotions.starts_at as promotion_starts_at',
             'promotions.ends_at as promotion_ends_at',
         )
-        
+
         ->join('category_products','category_products.child_category_id','child_categories.id')
         ->join('parent_categories','parent_categories.id','child_categories.parent_category_id')
         ->join('products', 'products.id', 'category_products.product_id')
         ->leftJoin('promotions', 'promotions.id','=','products.promotion_id')
 
         ->withCasts( $casts );
-        // ->get();
-
-        $pagination_data = $this->paginate_service->paginate_results($base_query);
+        
+        $pagination_data = $this->refine_service->refine_results($base_query, $data);
 
         $products = $pagination_data['products'];
         $paginate = $pagination_data['paginate'];
@@ -92,84 +92,10 @@ class CategoryService {
             $category['products'][] = $product;
         }
 
-        // $results['products'] = $pagination_data['products'];
-        // $results['paginate'] = ;
-
-        // $categories = [];
-
-        // foreach($products as $product){
-            
-        //     $this->promotion_service->set_product_promotion($product);
-
-        //     if(key_exists($product->child_category_id , $categories)){
-        //         $categories[$product->child_category_id]['products'][] = $product;
-        //     } else {
-        //         $categories[$product->child_category_id] = [
-        //             'id' => $product->child_category_id,
-        //             'name' => $product->child_category_name,
-        //             'parent_category_id' => $product->parent_category_id,
-        //             'store_type_id' => $product->store_type_id,
-        //             'products' => [$product]
-        //         ];
-        //     }
-        // }
-
         return $category;
 
     }
 
-    // public function grocery_categories($store_type_id){
-    //     $grand_parent_categories = GrandParentCategory::where('grand_parent_categories.store_type_id', $store_type_id)->get();
-
-    //     foreach($grand_parent_categories as $category){
-    //         $category->parent_categories;
-    //     }
-
-    //     return $grand_parent_categories;
-    // }
-
-    // public function grocery_products($parent_cateogy_id){
-
-    //     $product = new Product();
-    //     $casts = $product->casts;
-
-    //     $casts['category_name'] = HTMLDecode::class;
-        
-    //     $products = ChildCategory::where('child_categories.parent_category_id', $parent_cateogy_id)
-    //     ->join('category_products','category_products.child_category_id','child_categories.id')
-    //     ->join('parent_categories','parent_categories.id','child_categories.parent_category_id')
-    //     ->join('products', 'products.id', 'category_products.product_id')
-    //     ->select(
-    //         'products.*',
-    //         'child_categories.store_type_id as store_type_id',
-    //         'child_categories.id as child_category_id','child_categories.name as child_category_name',
-    //         'parent_categories.id as parent_category_id', 'parent_categories.name as parent_category_name'
-    //     )
-    //     ->withCasts( $casts )
-    //     ->get();
-
-    //     $categories = [];
-
-    //     foreach($products as $product){
-            
-    //         if(key_exists($product->child_category_id , $categories)){
-    //             $categories[$product->child_category_id]['products'][] = $product;
-    //         } else {
-    //             $categories[$product->child_category_id] = [
-    //                 'id' => $product->child_category_id,
-    //                 'name' => $product->child_category_name,
-    //                 'parent_category_id' => $product->parent_category_id,
-    //                 'store_type_id' => $product->store_type_id,
-    //                 'products' => [$product]
-    //             ];
-    //         }
-    //     }
-
-    //     $categories = array_values($categories);
-
-    //     return $categories;
-
-    // }
 
     public function featured($store_type_id){
 
--- app/Services/SearchService.php
+++ app/Services/SearchService.php
@@ -17,13 +17,13 @@ use Illuminate\Support\Facades\Redis;
 
 class SearchService {
 
-    private $client, $store_service, $promotion_service, $paginate_service;
+    private $client, $store_service, $refine_service;
 
-    public function __construct(StoreService $store_service, PaginateService $paginate_service){
+    public function __construct(StoreService $store_service, RefineService $refine_service){
         $this->client = ClientBuilder::create()->setRetries(3)->setHosts(['host' => env('ELASTICSEARCH_HOST')])->build();
        
         $this->store_service = $store_service;
-        $this->paginate_service = $paginate_service;
+        $this->refine_service = $refine_service;
 
         $this->promotion_service = new PromotionService();
     }
@@ -231,25 +231,7 @@ class SearchService {
                 $base_query = $this->search_where($type, $query, $store_type_id, $base_query);
             }   
 
-            if(!$text_search || ($text_search && $sort != '' && $order != '')){
-                $base_query = $this->search_sort($data, $base_query, $text_search);
-            } else {
-                $product_ids = join(',', $item_ids);
-                $base_query = $base_query->orderByRaw("FIELD(products.id, $product_ids)");
-            }
-
-            $base_query = $this->search_dietary($data, $base_query);
-            $base_query = $this->search_brand($data, $base_query);
-            $base_query = $this->search_category($data, $base_query);
-
-            $pagination_data = $this->paginate_service->paginate_results($base_query);
-
-            foreach($pagination_data['products'] as $product){
-                $this->promotion_service->set_product_promotion($product);
-            }
-            
-            $results['products'] = $pagination_data['products'];
-            $results['paginate'] = $pagination_data['paginate'];
+            $results = $this->refine_service->refine_results($base_query, $data, $item_ids);
 
             Redis::set($cache_key, json_encode($results));
             Redis::expire($cache_key, 86400);
@@ -305,77 +287,6 @@ class SearchService {
 
         return $base_query;
     }
-
-    //////////////////    Filter Results   //////////////////
-
-    private function search_sort($data, Builder $base_query){
-
-        $order_by_list = [];
-
-        if(key_exists('sort', $data) && key_exists('order', $data) && !is_null($data['order'])){
-            $sort = strtolower($data['sort']);
-            $order = strtoupper($data['order']);
-
-            if($order != 'ASC' && $order != 'DESC'){
-                throw new Exception('Unknown order by option.', 422);
-            }
-
-            $sort_options = [
-                'rating' => 'avg_rating',
-                'price' => 'price',
-            ];
-
-            if(key_exists($sort,$sort_options)){
-                $order_by_list[] = $sort_options[$sort] . ' '. $order;
-            } else {
-                throw new Exception('Unknown sort by option.', 422);
-            }
-
-        } else {
-            $order_by_list[] = 'total_reviews_count / avg_rating desc';
-        }
-        
-        $order_by_list[] = 'products.id asc';
-        
-        $base_query = $base_query->orderByRaw(join(',', $order_by_list));
-
-        return $base_query;
-    }
-
-    private function search_dietary($data, Builder $base_query){
-        if(key_exists('dietary', $data) && !is_null($data['dietary'])){
-            $dietary_list = explode(',',$data['dietary']);
-
-            foreach($dietary_list as $dietary){
-                $base_query = $base_query->where('dietary_info','like', "%$dietary%");
-            }
-            
-        }
-
-        return $base_query;
-    }
-
-    private function search_brand($data, Builder $base_query){
-        if(key_exists('brand', $data) && !is_null($data['brand'])){
-            $brand = $data['brand'];
-            $base_query = $base_query->where('brand',$brand);
-        }
-
-        return $base_query;
-    }
-
-    private function search_category($data, Builder $base_query){
-        if(key_exists('child_category', $data) && !is_null($data['child_category'])){
-            $category = $data['child_category'];
-            $base_query = $base_query->where('child_categories.name',$category);
-        }
-
-        return $base_query;
-    }
-
-    //////////////////    Filter Results   //////////////////
-
-
 
 
     ///////////////////////////////////////////     Results        ///////////////////////////////////////////
