f614a9bb8e4ab863e13f0763e4dbfc43a87a5e67
Zakariya Mohummed <zakaria2011@live.no>
2021-05-22 20:38:25 +0100

    Created Flyers Table - Migration File

---
+++ database/migrations/2021_05_22_130054_create_flyers_table.php
@@ -0,0 +1,52 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFlyersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('flyers', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('name');
+            $table->string('week')->nullable();
+
+            $table->string('url');
+
+            $table->unsignedBigInteger('store_id');
+            $table->unsignedBigInteger('store_type_id');
+
+            $table->date('valid_from');
+            $table->date('valid_to');
+
+            $table->string('site_flyer_id');
+
+            $table->unique(['name', 'site_flyer_id', 'store_id', 'valid_from', 'valid_to'],'flyer_unique');
+
+            $table->foreign('store_id')->references('id')->on('stores');
+            $table->foreign('store_type_id')->references('id')->on('store_types');
+
+            $table->timestamp('created_at')->useCurrent();
+            $table->timestamp('updated_at')->default(DB::raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'));
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('flyers');
+    }
+}
