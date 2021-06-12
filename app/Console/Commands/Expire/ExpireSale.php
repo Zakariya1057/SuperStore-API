a22e46e493fa9fa9c77bb961e8ab3f559c3ab81f
Zakariya Mohummed <zakaria2011@live.no>
2021-06-12 10:38:40 +0100

    Expire sale, region improvements

--- app/Console/Commands/Expire/ExpireSale.php
+++ app/Console/Commands/Expire/ExpireSale.php
@@ -5,10 +5,13 @@ namespace App\Console\Commands\Expire;
 use App\Events\GroceryListChangedEvent;
 use App\Models\GroceryList;
 use App\Models\GroceryListItem;
-use App\Models\Product;
-use Carbon\Carbon;
-use Illuminate\Console\Command;
+use App\Models\ProductPrice;
+use App\Models\User;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Console\Command;
+use Carbon\Carbon;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 
 class ExpireSale extends Command
 {
@@ -46,14 +49,21 @@ class ExpireSale extends Command
         // Fetch all expired promotions, get all matching products remove product_id and then delete them
         $this->info('Fetching All Expired Sales');
 
-        $products = Product::whereDate('sale_ends_at', '<', Carbon::now())->get();
+        DB::beginTransaction();
+
+        $product_prices = ProductPrice::where('sale_ends_at', '<', Carbon::now())
+        ->select('products.*', 'product_prices.id as product_price_id', 'product_prices.sale_ends_at', 'product_prices.price', 'product_prices.old_price')
+        ->join('products', 'products.id', 'product_prices.product_id')
+        ->get();
 
         $product_ids = [];
-        $products_count = count($products);
+        $count = count($product_prices);
 
-        $this->info($products_count . ' Products with expired sales');
+        $this->info($count . ' Product prices with expired sales');
 
-        foreach($products as $product){
+        foreach($product_prices as $product){
+            $product_price_id = $product->product_price_id;
+
             $product_id = $product->id;
             $name = $product->name;
             $ends_at = $product->sale_ends_at;
@@ -61,11 +71,11 @@ class ExpireSale extends Command
             $price = $product->price;
             $old_price = $product->old_price;
 
-            $this->info("Expired Product Sale Found: $ends_at [$product_id] $name - $price -> $old_price");
+            $this->info("Expired Sale Found: $ends_at [$product_id] $name - $price -> $old_price");
 
             $product_ids[] = $product_id;
 
-            Product::where('id', $product_id)->update([
+            ProductPrice::where('id', $product_price_id)->update([
                 'price' => $old_price, 
                 'old_price' => null,
                 'sale_ends_at' => null,
@@ -75,19 +85,25 @@ class ExpireSale extends Command
 
         $this->info('Finding Grocery Lists Containing Product');
 
-        $list_ids = GroceryListItem::whereIn('product_id', $product_ids)->groupBy('grocery_list_items.list_id')->pluck('grocery_list_items.list_id');
+        $list_ids = GroceryListItem::whereIn('product_id', array_unique($product_ids))->groupBy('grocery_list_items.list_id')->pluck('grocery_list_items.list_id');
 
         $lists = GroceryList::whereIn('id', $list_ids)->get();
 
         foreach($lists as $list){
             $this->info('List Found Using Product: ' . $list->id);
+
+            $user = User::where('id', $list->user_id)->get()->first();
+            Auth::login($user);
+
             event(new GroceryListChangedEvent($list));
         }
 
-        if($products_count > 0){
+        if($count > 0){
             Artisan::call('cache:home');
         }
         
+        DB::commit();
+
     }
 
 }
