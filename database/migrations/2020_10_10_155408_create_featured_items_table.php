d9732a3be5bccff712cf6db3373869910279c63c
Zakariya Mohummed <zakaria2011@live.no>
2021-06-16 20:55:30 +0100

    Featured Items: Added regions id field

--- database/migrations/2020_10_10_155408_create_featured_items_table.php
+++ database/migrations/2020_10_10_155408_create_featured_items_table.php
@@ -19,6 +19,8 @@ class CreateFeaturedItemsTable extends Migration
 
             $table->unsignedBigInteger('featured_id');
             $table->string('type');
+
+            $table->unsignedBigInteger('region_id');
             $table->unsignedBigInteger('store_type_id');
 
             $table->integer('week');
@@ -28,8 +30,9 @@ class CreateFeaturedItemsTable extends Migration
             $table->index('year');
             $table->index('type');
 
+            $table->foreign('region_id')->references('id')->on('regions');
             $table->foreign('store_type_id')->references('id')->on('store_types');
-            $table->unique(['store_type_id', 'featured_id', 'type']);
+            $table->unique(['store_type_id', 'featured_id', 'type', 'region_id']);
 
             $table->timestamp('created_at')->useCurrent();
             $table->timestamp('updated_at')->default(DB::raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'));
